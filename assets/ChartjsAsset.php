fd4ae37c190ed508359e386fbd20b9a1c7e196ca
MatveevaElena <wkatara@gmail.com>
2019-12-25 09:53:24 +0400

    Init

---
+++ assets/ChartjsAsset.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ChartjsAsset extends AssetBundle
+{
+    public $sourcePath = '@app/components/chartjs';
+    public $css = [
+        'css/Chart.min.css',
+    ];
+    public $js = [
+        'js/Chart.min.js',
+    ];
+    public $jsOptions = ['position' => \yii\web\View::POS_END];
+}
